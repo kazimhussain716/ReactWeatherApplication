dd5031ee8620bce88d55ba6a9c33157358308777
Kazim Hussain <kazimhussain685@gmail.com>
2017-06-02 14:48:57 +0500

    AddedCopy

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -9,7 +9,10 @@ var React = require('react');
 // });
 var Example = (props)=>{
   return (
+    <div>
          <h2>Example Component!</h2>
+         <p>Welcome to Example Page!</p>
+    </div>
       );
 }
 module.exports=Example;
