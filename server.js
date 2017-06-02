2ed24ada818c8dfccd8a2d7af400e2f4cdb93ab7
Kazim Hussain <kazimhussain685@gmail.com>
2017-06-02 15:37:53 +0500

    update middleware for localhost

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var app=express();
 
 const PORT = process.env.PORT || 3000;
 app.use(function (request,response,next){
-  if (request.headers['x-forwarded-proto']==='http'){
-     next();
+  if (request.headers['x-forwarded-proto']==='https'){
+    response.redirect('http://'+request.hostname+request.url);
+     
   }
   else{
-    response.redirect('http://'+request.hostname+request.url);
+    next();
   }
 })
 //Folder to serve
